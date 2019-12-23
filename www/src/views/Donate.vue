b9b7213c50de1f89cd943ea5c4f0a9b5d4eb377d
OriginalYellow <michael.robert.joseph1@gmail.com>
2019-12-23 18:38:18 -0500

    update donate page donation button

--- www/src/views/Donate.vue
+++ www/src/views/Donate.vue
@@ -20,10 +20,23 @@ breakpoint is hit -->
           <p :class="titleClass">
             {{ 'Ways to Donate' }}
           </p>
-          <p>
-            <a
+          <p>Click here to donate through our paypal:</p>
+          <p class="mb-5">
+            <v-btn
+              depressed
+              large
+              color="accentLightest"
+              class="greyDark--text cta"
+              :ripple="{ class: 'primaryLight--text' }"
               href="https://www.paypal.me/beyondcompassion"
-            >Click here to donate through our paypal</a>
+              :loading="loading"
+              @click="loading = !loading"
+            >
+              <v-img
+                width="90"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/PayPal.svg/1280px-PayPal.svg.png"
+              />
+            </v-btn>
           </p>
         </v-flex>
       </v-layout>
@@ -40,6 +53,7 @@ breakpoint is hit -->
 
     // MIKE: put the "projects" array into the localization file
     data: () => ({
+      loading: false,
       // projects: [
       //   {
       //     name: 'some charity project',
@@ -72,7 +86,8 @@ breakpoint is hit -->
         return {
           'display-1': this.$vuetify.breakpoint.xs,
           'display-3': this.$vuetify.breakpoint.smAndUp,
-          'font-weight-thin': true
+          'font-weight-thin': true,
+          'mb-5': true
         }
       }
     }
